5abbbb9256e9033d279d5fd238f673d178421a42
Frenco <hey@frenco.dev>
2021-02-20 11:49:59 +0630

    feat: add more components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,9 @@
-import { Button, Tooltip } from '@geist-ui/react'
-import Moon from '@geist-ui/react-icons/moon'
-import Sun from '@geist-ui/react-icons/sun'
-import { useAtom } from 'jotai'
 import { NextPage } from 'next'
 
-import { themeAtom } from '../state/atoms'
+import { Layout } from '../components/Layout'
 
 const IndexPage: NextPage = () => {
-  const [theme, setTheme] = useAtom(themeAtom)
-  return (
-    <main className="flex items-center justify-center w-full h-screen">
-      <Tooltip text="Theme">
-        <Button
-          icon={theme == 'light' ? <Sun /> : <Moon />}
-          type="secondary"
-          size="large"
-          auto
-          ghost
-          onClick={() => setTheme(theme == 'light' ? 'dark' : 'light')}
-          className="p-0 border-2 rounded-full w-11"
-        />
-      </Tooltip>
-    </main>
-  )
+  return <Layout>Index Page</Layout>
 }
 
 export default IndexPage
