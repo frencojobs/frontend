9cbfc3fb9da02a1a3f5dfdbc284edb99b712c9dd
Frenco <hey@frenco.dev>
2021-02-20 11:49:50 +0630

    feat: add next-seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,16 @@ import '../styles/tailwind.css'
 import { CssBaseline, GeistProvider } from '@geist-ui/react'
 import { Provider as JotaiRoot } from 'jotai'
 import { useAtom } from 'jotai'
+import { DefaultSeo } from 'next-seo'
 import { AppProps } from 'next/app'
 
+import SEO from '../lib/seo.config'
 import { themeAtom } from '../state/atoms'
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
   <JotaiRoot>
     <GeistApp>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </GeistApp>
   </JotaiRoot>
